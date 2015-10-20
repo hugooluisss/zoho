64648cd4a31501b4018ca65303e9baac52672cf8
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2015-10-20 00:54:59 -0500

    Actualización para conexión con otro token

--- prueba.php
+++ prueba.php
@@ -1,17 +1,18 @@
 <?php
 
 header("Content-type: application/xml");
-$token="9882dda954f23d4da680de806293f912";
+#$token="9882dda954f23d4da680de806293f912";
+$token="f19ad4cf9ecfb802a8da2254bfa6b43d";
 $url = "https://crm.zoho.com/crm/private/xml/Leads/insertRecords";
 
 $xml = '
 <Leads>
 <row no="1">
 <FL val="Lead Source">Web Download</FL>
-<FL val="Company">Huguntu</FL>
-<FL val="First Name">Huguntu</FL>
+<FL val="Company">Huguntu 6</FL>
+<FL val="First Name">Huguntu 6</FL>
 <FL val="Last Name">Smith</FL>
-<FL val="Email">testing@testing.com</FL>
+<FL val="Email">testing@testing6.com</FL>
 <FL val="Title">Manager</FL>
 <FL val="Phone">1234567890</FL>
 <FL val="Home Phone">0987654321</FL>
@@ -20,11 +21,15 @@ $xml = '
 <FL val="Mobile">292827622</FL>
 <FL val="Lugar de preferencia del Evento">jajajaja</FL>
 <FL val="Horario probable del evento">jajajaja</FL>
+<FL val="Lugar de preferencia del Evento">Salón Altea</FL>
+<FL val="Lead Owner">Melany Zelada</FL>
+<FL val="LEADCF1">Salón Altea</FL>
 </row>
 </Leads>
 ';
 
-$param= "newFormat=1&authtoken=".$token."&scope=crmapi&xmlData={$xml}";
+#Sal&oacute;n&#x20;Altea&#x20;
+$param= "newFormat=1&wfTrigger=true&authtoken=".$token."&scope=crmapi&xmlData={$xml}";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
